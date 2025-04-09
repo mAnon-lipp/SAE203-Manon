2fd59ba81c0f4ac65736d9ac00b99b17e71cdab5
Lippler Manon <lippler1@mmi-dev.unilim.fr>
2025-04-09 13:30:06 +0200

    css

--- app/component/featuredMovies/script.js
+++ app/component/featuredMovies/script.js
@@ -1,23 +1,35 @@
 let templateFile = await fetch("./component/featuredMovies/template.html");
 let template = await templateFile.text();
 
+let templateAllFile = await fetch("./component/featuredMovies/templateAll.html");
+let templateAll = await templateAllFile.text();
+
 let FeaturedMovies = {};
 
+// Formate un seul film en utilisant template.html
+FeaturedMovies.formatOne = function (movie) {
+  let movieHtml = template;
+  movieHtml = movieHtml.replace("{{image}}", movie.image);
+  movieHtml = movieHtml.replace("{{title}}", movie.name);
+  movieHtml = movieHtml.replace("{{description}}", movie.description);
+  movieHtml = movieHtml.replace("{{onclick}}", `C.handlerDetail(${movie.id})`);
+  return movieHtml;
+};
+
+// Formate une liste de films en utilisant templateAll.html
 FeaturedMovies.format = function (movies) {
   if (movies.length === 0) {
     return "<p>Aucun film mis en avant pour le moment.</p>";
   }
 
-let formattedMovies = "";
-for (let movie of movies) {
-    let movieHtml = template;
-    movieHtml = movieHtml.replace("{{image}}", movie.image);
-    movieHtml = movieHtml.replace("{{title}}", movie.name);
-    movieHtml = movieHtml.replace("{{description}}", movie.description);
-    movieHtml = movieHtml.replace("{{onclick}}", `C.handlerDetail(${movie.id})`);
-    formattedMovies += movieHtml;
-}
-return formattedMovies;
+  let formattedMovies = "";
+  for (let movie of movies) {
+    formattedMovies += FeaturedMovies.formatOne(movie);
+  }
+
+  let allMoviesHtml = templateAll;
+  allMoviesHtml = allMoviesHtml.replace("{{movies}}", formattedMovies);
+  return allMoviesHtml;
 };
 
 export { FeaturedMovies };
--- app/component/film/script.js
+++ app/component/film/script.js
@@ -1,38 +1,54 @@
 let templateFile = await fetch("./component/film/template.html");
 let template = await templateFile.text();
 
+let templateAllFile = await fetch("./component/film/templateAll.html");
+let templateAll = await templateAllFile.text();
+
 let Movie = {};
 
-Movie.format = function (movies, profileId, favorites) {
+// Formate un seul film en utilisant template.html
+Movie.formatOne = function (movie, profileId, favorites) {
+  let movieHtml = template;
+  movieHtml = movieHtml.replace("{{titre}}", movie.name);
+  movieHtml = movieHtml.replace("{{image}}", movie.image);
+  movieHtml = movieHtml.replace("{{onclick}}", `C.handlerDetail(${movie.id})`);
 
-  let html = "";
-  let i = 0;
-
-  // Ensure favorites is an array
-  favorites = Array.isArray(favorites) ? favorites : [];
-
-  while (i < movies.length) {
-    let movie = movies[i];
-    let movieHtml = template;
-    movieHtml = movieHtml.replace("{{titre}}", movie.name);
-    movieHtml = movieHtml.replace("{{image}}", movie.image);
-    movieHtml = movieHtml.replace("{{onclick}}", `C.handlerDetail(${movie.id})`);
-    let isFavorite = false;
-    for (let fav of favorites) {
-      if (fav.id === movie.id) {
-        isFavorite = true;
-        break;
-      }
-    }
-    const favoriteButton = isFavorite
-      ? `<button disabled>Favori</button>`
-      : `<button onclick="C.addFavorite(${profileId}, ${movie.id})">Ajouter aux favoris</button>`;
-
-    movieHtml += favoriteButton;
-    html += movieHtml;
-    i++;
+  // Vérifie si favorites est défini et initialise si nécessaire
+  if (!favorites || !Array.isArray(favorites)) {
+    favorites = [];
   }
-  return html;
+
+  // Vérifie si le film est dans les favoris
+  let isFavorite = false;
+  for (let i = 0; i < favorites.length; i++) {
+    if (favorites[i].id === movie.id) {
+      isFavorite = true;
+      break;
+    }
+  }
+
+  const favoriteButton = isFavorite
+    ? `<button disabled>Favori</button>`
+    : `<button onclick="C.addFavorite(${profileId}, ${movie.id})">Ajouter aux favoris</button>`;
+
+  movieHtml = movieHtml.replace("{{button}}", favoriteButton);
+  return movieHtml;
+};
+
+// Formate une liste de films en utilisant templateAll.html
+Movie.format = function (movies, profileId, favorites) {
+  if (!movies || movies.length === 0) {
+    return "<p>Aucun film disponible pour le moment.</p>";
+  }
+
+  let formattedMovies = "";
+  for (let i = 0; i < movies.length; i++) {
+    formattedMovies += Movie.formatOne(movies[i], profileId, favorites);
+  }
+
+  let allMoviesHtml = templateAll;
+  allMoviesHtml = allMoviesHtml.replace("{{movies}}", formattedMovies);
+  return allMoviesHtml;
 };
 
 export { Movie };
